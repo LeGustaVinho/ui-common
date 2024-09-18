1d062a62ff7c38d69e5f841cdc3b2cb848788818
Gustavo <gustavo.oliveaxe@gmail.com>
2024-09-18 07:54:54 -0300

    Improved GameObjectListing

--- GameObjectListing.cs
+++ GameObjectListing.cs
@@ -5,16 +5,29 @@ using Object = UnityEngine.Object;
 
 namespace LegendaryTools.UI
 {
+    [Serializable]
     public class GameObjectListing<TGameObject, TData>
         where TGameObject : Component, GameObjectListing<TGameObject, TData>.IListingItem
+        where TData : IEquatable<TData>
     {
+        public interface IListingItem
+        {
+            void Init(TData item);
+            void Update(TData item);
+        }
+        
         public readonly Func<TData[]> DataProvider;
 
-        public bool AutoDestroyAllBeforeAdd = true;
+        public bool ForceDestroyBeforeAdd;
         public List<TGameObject> Listing = new List<TGameObject>();
         public Transform Parent;
         public TGameObject Prefab;
+        
         private Transform prefabTransform;
+
+        private readonly Dictionary<TData, TGameObject> gameObjectTable = new Dictionary<TData, TGameObject>();
+        
+        public event Action<TGameObject> OnPreDestroy;
 
         public GameObjectListing()
         {
@@ -26,81 +39,96 @@ namespace LegendaryTools.UI
             Parent = parent;
             DataProvider = dataProvider;
         }
-
-        public event Action<TGameObject> OnPreDestroy;
-
+        
         public virtual List<TGameObject> Generate()
         {
             return DataProvider != null ? GenerateList(DataProvider.Invoke()) : null;
         }
 
-        public virtual List<TGameObject> GenerateList(TData[] itens, Predicate<TData> filter = null)
+        public virtual List<TGameObject> GenerateList(TData[] items, Predicate<TData> filter = null)
         {
-            if (AutoDestroyAllBeforeAdd)
-            {
-                DestroyAll();
-            }
+            if (ForceDestroyBeforeAdd) DestroyAll();
 
-            for (int i = 0; i < itens.Length; i++)
+            foreach (TData currentItem in items)
             {
                 if (filter != null)
                 {
-                    if (filter.Invoke(itens[i]))
-                    {
-                        CreateGameObject(itens[i]);
-                    }
+                    if (filter.Invoke(currentItem))
+                        CreateOrUpdate(currentItem);
                 }
                 else
-                {
-                    CreateGameObject(itens[i]);
-                }
+                    CreateOrUpdate(currentItem);
+            }
+
+            if (ForceDestroyBeforeAdd) return Listing;
+            
+            HashSet<TData> updatedItemCollection = new HashSet<TData>(items);
+            foreach (KeyValuePair<TData, TGameObject> pair in gameObjectTable)
+            {
+                if (!updatedItemCollection.Contains(pair.Key))
+                    Destroy(pair.Key);
             }
 
             return Listing;
+        }
+
+        private void CreateOrUpdate(TData currentItem)
+        {
+            if (gameObjectTable.TryGetValue(currentItem, out TGameObject go) && !ForceDestroyBeforeAdd)
+                go.Update(currentItem);
+            else
+                CreateGameObject(currentItem);
         }
 
         public virtual void DestroyAll()
         {
             for (int i = 0; i < Listing.Count; i++)
             {
-                if (Listing[i] != null)
-                {
-                    OnPreDestroy?.Invoke(Listing[i]);
-                    Object.Destroy(Listing[i].gameObject);
-                }
+                if (Listing[i] == null) continue;
+                OnPreDestroy?.Invoke(Listing[i]);
+                Object.Destroy(Listing[i].gameObject);
             }
 
             Listing.Clear();
+            gameObjectTable.Clear();
+        }
+
+        public virtual void Destroy(TData item)
+        {
+            if (item == null) return;
+            if (gameObjectTable.TryGetValue(item, out TGameObject go))
+            {
+                OnPreDestroy?.Invoke(go);
+                Listing.Remove(go);
+                Object.Destroy(go);
+            }
+            gameObjectTable.Remove(item);
         }
 
         protected virtual TGameObject CreateGameObject(TData item)
         {
-            TGameObject newGO = InstantiateFromPrefab(item, Prefab);
-            Transform newGOTransform = newGO.transform;
+            TGameObject newGameObject = InstantiateFromPrefab(item, Prefab);
+            Transform goTransform = newGameObject.transform;
             if (prefabTransform == null)
             {
                 prefabTransform = Prefab.transform;
             }
 
-            Listing.Add(newGO);
+            Listing.Add(newGameObject);
+            gameObjectTable.Add(item, newGameObject);
 
-            newGOTransform.SetParent(Parent);
-            newGOTransform.localPosition = prefabTransform.localPosition;
-            newGOTransform.localScale = prefabTransform.localScale;
-            newGOTransform.localRotation = prefabTransform.localRotation;
-            newGO.Init(item);
+            goTransform.SetParent(Parent);
+            goTransform.localPosition = prefabTransform.localPosition;
+            goTransform.localScale = prefabTransform.localScale;
+            goTransform.localRotation = prefabTransform.localRotation;
+            newGameObject.Init(item);
 
-            return newGO;
+            return newGameObject;
         }
 
         protected virtual TGameObject InstantiateFromPrefab(TData item, TGameObject prefab)
         {
             return Object.Instantiate(Prefab);
-        }
-
-        public interface IListingItem
-        {
-            void Init(TData item);
         }
     }
 }
