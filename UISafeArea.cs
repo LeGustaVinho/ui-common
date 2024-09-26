4484a97311c832ad8cbf78a7b60cec50a6a34205
Gustavo <gustavo.oliveaxe@gmail.com>
2024-09-25 21:57:14 -0300

    Added safe area

---
+++ UISafeArea.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace LegendartTools.UI
+{
+    public class UISafeArea : MonoBehaviour
+    {
+        private RectTransform rectTransform;
+
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.ShowInInspector]
+#endif
+        private Rect lastSafeArea = Rect.zero;
+
+        void Start()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            ApplySafeArea();
+        }
+
+        void Update()
+        {
+            ApplySafeArea();
+        }
+
+        private void ApplySafeArea()
+        {
+            Rect safeArea = UnityEngine.Screen.safeArea;
+            if (safeArea != lastSafeArea)
+            {
+                lastSafeArea = safeArea;
+
+                Vector2 anchorMin = safeArea.position;
+                Vector2 anchorMax = safeArea.position + safeArea.size;
+                anchorMin.x /= UnityEngine.Screen.width;
+                anchorMin.y /= UnityEngine.Screen.height;
+                anchorMax.x /= UnityEngine.Screen.width;
+                anchorMax.y /= UnityEngine.Screen.height;
+                
+                rectTransform.anchorMin = anchorMin;
+                rectTransform.anchorMax = anchorMax;
+            }
+        }
+    }
+}
